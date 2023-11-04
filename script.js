1cb6d834d694a0a34c4c91d4400d365b69266ebf
Alsaze <nadar.veselov@mail.ru>
2023-11-04 17:01:04 +0300

    Block-fields refactoring

--- script.js
+++ script.js
@@ -1,39 +1,47 @@
-profileName = document.getElementById("register-input-profileName");
-mail = document.getElementById("register-input-mail");
-password = document.getElementById("register-input-password");
+function passwordChange(id) {
+    const parent = document.getElementById(id).parentElement;
+    const children = parent.children;
 
-const inputProfileName = document.getElementById("register-block-field-profileName");
-const inputMail = document.getElementById("register-block-field-profileMail");
-const inputPassword = document.getElementById("register-block-field-profilePassword");
-
-const errorProfileName = document.getElementById("register-block-error-message-profileName");
-const errorMail = document.getElementById("register-block-error-message-mail");
-const errorPassword = document.getElementById("register-block-error-message-password");
-
-function passwordChange() {
-    password.type === "text" ? password.type = "password" : password.type = "text";
+    const input = document.getElementById(children[1].id);
+    input.type === "text" ? input.type = "password" : input.type = "text";
 }
 
-function onchangeRegisterInputProfileName() {
-    const bool = profileName.value === undefined || profileName.value === '';
-    wrongField(inputProfileName, bool);
+function onchangeInput(id) { //id = login-block-mail
+    const children = document.getElementById(id).children;
+    const label = document.getElementById(children[0].id); //login-block-field-profileMail
+
+    const error = document.getElementById(children[1].id); //login-block-error-message-mail
+
+    const childrenLabel = document.getElementById(children[0].id).children;
+    const input = document.getElementById(childrenLabel[1].id); //login-input-mail
+
+    const bool = input.value === undefined || input.value === '';
+    wrongField(label, bool);
     const message = "You must fill this field";
-    createWrongMessage(errorProfileName, message, bool);
+    createWrongMessage(error, message, bool);
+
+    if (input.id === "login-input-mail" || "register-input-mail"){
+
+        if (bool) return;
+
+        if (validateEmail(input.value)) {
+            wrongField(label, false);
+
+            createWrongMessage(error, message, false);
+        } else {
+            wrongField(label, true);
+
+            const message = input.value + " is not correct";
+            createWrongMessage(error, message, true);
+        }
+    }
 }
 
-function onchangeRegisterInputProfileMail() {
-    const bool = mail.value === undefined || mail.value === '';
-    wrongField(inputMail, bool);
-    const message = "You must fill this field";
-    createWrongMessage(errorMail, message, bool);
+function validateEmail(email) {
+    let re = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+    return re.test(String(email).toLowerCase());
 }
 
-function onchangeRegisterInputProfilePassword() {
-    const bool = password.value === undefined || password.value === '';
-    wrongField(inputPassword, bool);
-    const message = "You must fill this field";
-    createWrongMessage(errorPassword, message, bool);
-}
 
 function wrongField(field, bool = false) {
     field.classList.toggle("register-block-error", bool);
@@ -45,33 +53,9 @@ function createWrongMessage(error, message, bool = false) {
 
 function register() {
     const userRegisterData = {
-        name: profileName.value,
-        mail: mail.value,
-        password: password.value
+
     }
     let json = JSON.stringify(userRegisterData);
 
     //тут вызываем метод который отправляет userRegisterData на бэк
-}
-
-function backendConfirm() {
-    //переходим на главную страницу
-}
-
-function backendCancelName() {
-    wrongField(inputProfileName);
-    const message = "This name is already reserved";
-    createWrongMessage(errorProfileName, message, true);
-}
-
-function backendCancelMail() {
-    wrongField(inputMail);
-    const message = "This mail is already reserved";
-    createWrongMessage(errorMail, message, true);
-}
-
-function backendCancelPassword() {
-    wrongField(inputPassword);
-    const message = "This password is bad";
-    createWrongMessage(errorPassword, message, true);
 }
